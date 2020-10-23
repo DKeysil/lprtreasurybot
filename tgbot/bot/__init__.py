7155b77e9a8b1db941fe5b25ceca5c89c0cd782b
dkisel <dkisel@emphasoft.com>
2020-10-23 13:48:14 +0300

    Now treasurer can set allowed chats. Bot will answer only in allowed chats.

--- tgbot/bot/__init__.py
+++ tgbot/bot/__init__.py
@@ -2,6 +2,9 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
 import os
+from aiogram.dispatcher.middlewares import BaseMiddleware
+from aiogram.dispatcher.handler import CancelHandler
+from motor_client import SingletonClient
 
 API_TOKEN = os.environ['BOT_API_KEY']
 
@@ -9,5 +12,42 @@ bot = Bot(token=API_TOKEN)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 
+
+class ChatCheckerMiddleware(BaseMiddleware):
+
+    def __init__(self):
+        super(ChatCheckerMiddleware, self).__init__()
+
+    async def on_process_message(self, message: types.Message, data: dict):
+        if message.get_command() == '/get_me':
+            return
+
+        db = SingletonClient.get_data_base()
+        allowed_chats = []
+
+        settings = await db.settings.find_one({
+            "id": 0
+        })
+        if settings:
+            if settings.get('allowed_chats'):
+                allowed_chats += settings.get('allowed_chats')
+
+                if os.environ.get('ALLOWED_CHATS'):
+                    environ_chats = list(map(int, os.environ['ALLOWED_CHATS'].split(',')))
+                    allowed_chats += environ_chats
+
+                treasurers_cursor = db.users.find({
+                    "treasurer": True
+                })
+
+                treasurers = await treasurers_cursor.to_list(length=await db.users.count_documents({}))
+                for treasurer in treasurers:
+                    allowed_chats.append(treasurer.get('user_id'))
+
+                if message.chat.id not in allowed_chats:
+                    raise CancelHandler()
+
+
+dp.middleware.setup(ChatCheckerMiddleware())
 
 from bot import modules
--- tgbot/bot/modules/settings/Settings.py
+++ tgbot/bot/modules/settings/Settings.py
@@ -8,6 +8,7 @@ from bot.modules.menu.Menu import menu
 class Settings(StatesGroup):
     settings_menu = State()
     preffered_fund = State()
+    allowed_chats = State()
 
 
 @dp.message_handler(lambda message: message.chat.type == 'private', commands=['settings'])
@@ -29,9 +30,10 @@ async def settings(message: types.Message):
         await Settings.settings_menu.set()
 
         preffered_fund_button = types.KeyboardButton('Предпочтительный фонд')
+        allowed_chats = types.KeyboardButton('Разрешенные чаты')
         back_button = types.KeyboardButton('Назад')
         btn_list = [
-            [preffered_fund_button],
+            [preffered_fund_button, allowed_chats],
             [back_button]
         ]
         settings_keyboard_markup = types.ReplyKeyboardMarkup(btn_list)
@@ -111,3 +113,84 @@ async def back_action(message: types.Message, state: FSMContext):
     """
     await state.finish()
     await menu(message)
+
+
+@dp.message_handler(lambda message: message.text == 'Разрешенные чаты', state=[Settings.settings_menu, Settings.allowed_chats])
+async def allowed_chats(message: types.Message, state: FSMContext):
+    """
+    Logic to set allowed chats.
+
+    Args:
+        message (types.Message): telegram message
+        state (FSMContext): state memory
+    """
+    await Settings.allowed_chats.set()
+
+    db = SingletonClient.get_data_base()
+    chats = []
+
+    _settings = await db.settings.find_one({
+        "id": 0
+    })
+    if _settings:
+        if _settings.get('allowed_chats'):
+            chats += [str(i) for i in _settings.get('allowed_chats')]
+
+    string = ''
+    if chats:
+        string += 'Сейчас для использования доступны эти чаты:\n\n'
+        string += ', '.join(chats)
+    remove_keyboard = types.ReplyKeyboardRemove()
+    if string:
+        await message.answer(string, reply_markup=remove_keyboard)
+
+    string = 'Пришлите полный список доступных чатов, разделенные через запятую.\n'
+    string += 'Например:\n379542159, 246256258, -1001430465521'
+    string += '\nЧтобы разрешить все чаты пришлите: -'
+
+    await message.answer(string, reply_markup=remove_keyboard)
+
+
+@dp.message_handler(state=Settings.allowed_chats)
+async def set_allowed_chats(message: types.Message, state: FSMContext):
+    """
+    Set allowed chats in DB
+
+    Args:
+        message (types.Message): telegram message
+        state (FSMContext): state memory
+
+    Returns:
+        [type]: calls settings menu
+    """
+    db = SingletonClient.get_data_base()
+
+    if message.text == '-':
+        result = await db.settings.update_one({"id": 0}, {"$set": {"allowed_chats": []}})
+        print('allowed_chats. update allowed chats:\n' + str(result.raw_result))
+
+        if result.modified_count != 1:
+            result = await db.settings.insert_one({"id": 0, "allowed_chats": []})
+            print('allowed_chats. insert allowed chats id:\n' +
+                  str(result.inserted_id))
+        await message.answer('Теперь бот доступен из любого чата.')
+        return await settings(message)
+
+    try:
+        chats = list(map(int, message.text.split(', ')))
+    except ValueError:
+        chats = []
+        print('allowed_chats. ValueError while parse funds list')
+
+    if chats:
+        result = await db.settings.update_one({"id": 0}, {"$set": {"allowed_chats": chats}})
+        print('allowed_chats. update allowed chats:\n' + str(result.raw_result))
+
+        if result.modified_count != 1:
+            result = await db.settings.insert_one({"id": 0, "allowed_chats": chats})
+            print('allowed_chats. insert allowed chats id:\n' +
+                  str(result.inserted_id))
+        await message.answer('Для бота были установлены доступные чаты.')
+        return await settings(message)
+
+    return await message.answer('Неправильный формат данных. Если вы хотите из этого меню, напишите /cancel.')
